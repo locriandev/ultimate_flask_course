364995968519ef706f9a8ef51fe842338d3b05a7
Daniele Paolella <daniele.paolella@konicaminolta.it>
2020-11-16 18:01:59 +0100

    Record new food into database using form data

--- food_tracker_app/modules/food.py
+++ food_tracker_app/modules/food.py
@@ -1,7 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from modules import database
 
 
 def init(app: Flask):
     @app.route('/food', methods=['GET', 'POST'])
     def _food():
+        if request.method == 'POST':
+            handle(request.form)
+            return redirect(url_for('_food'))
         return render_template('add_food.html')
+
+
+def handle(form):
+    food = {
+        'name': form['food'],
+        'protein': int(form['protein']),
+        'carbs': int(form['carbs']),
+        'fat': int(form['fat'])
+    }
+    food['calories'] = compute_calories(food)
+    add_food_to_db(food)
+
+
+def compute_calories(food):
+    return food['protein'] * 4 + food['carbs'] * 4 + food['fat'] * 9
+
+
+def add_food_to_db(food):
+    food_db = database.get_db()
+    food_db.execute(
+        'insert into food '
+        '(name, protein, carbohydrates, fats, calories) '
+        'values(?, ?, ?, ?, ?) ',
+        [food["name"], food["protein"], food["carbs"],
+         food["fat"], food["calories"]]
+    )
+    food_db.commit()
