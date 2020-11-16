acd5dd290927539b48ae4a24a76198da3f7f3c49
Daniele Paolella <daniele.paolella@konicaminolta.it>
2020-11-16 23:53:20 +0100

    Add food names to day detail dropdown

--- food_tracker_app/modules/details.py
+++ food_tracker_app/modules/details.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
-from modules import dates
+from modules import dates, food
 
 
 def init(app: Flask):
     @app.route('/details/<int:db_date>')
     def _details(db_date):
         pretty_date = dates.pretty_date(db_date)
-        return render_template('day.html', date=pretty_date)
+        return render_template(
+            'day.html',
+            date=pretty_date,
+            food_list=food.get_food()
+        )
